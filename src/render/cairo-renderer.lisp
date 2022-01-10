cfc20d9cb1daa4ce363af6f067bd3898f52d000f
Andy Best <andy.best@photobox.com>
2022-01-10 18:04:39 +0000

    Added more drawing calls to cairo renderer

--- src/render/cairo-renderer.lisp
+++ src/render/cairo-renderer.lisp
@@ -10,6 +10,8 @@
     (make-instance 'cairo-renderer
                    :context (c2:create-pdf-context output-path width height)))
 
+;;; Internal utilities
+
 (defun update-stroke-color (cr)
   (with-aref (r g b a) (stroke-color cr)
              (c2:set-source-rgba r g b a (cr-context cr))))
@@ -33,6 +35,15 @@
                            (update-stroke-color cr)
                            (c2:stroke (cr-context cr))))))
 
+
+;;; Renderer attributes
+
+(defmethod set-line-cap ((cr cairo-renderer) cap-type)
+  (c2:set-line-cap cap-type (cr-context cr)))
+
+(defmethod set-line-join ((cr cairo-renderer) join-type)
+  (c2:set-line-join join-type (cr-context cr)))
+
 (defmethod set-blend-mode ((cr cairo-renderer) mode)
   (c2:set-operator (ecase mode
                      (:no-blend :source)
@@ -44,23 +55,88 @@
   (vector (c2:width (cr-context cr))
           (c2:height (cr-context cr))))
 
+(defmethod push-state ((cr cairo-renderer))
+  (c2:save (cr-context cr)))
+
+(defmethod pop-state ((cr cairo-renderer))
+  (c2:restore (cr-context cr)))
+
+
+;;; 2D Primitives
+
 (defmethod point ((cr cairo-renderer) x y)
-  (update-fill-color cr)
-  (c2:rectangle x y 1 1 (cr-context cr))
-  (c2:fill-path (cr-context cr)))
+  (when (stroke-enabled cr)
+    (update-fill-color cr)
+    (c2:rectangle x y 1 1 (cr-context cr))
+    (c2:fill-path (cr-context cr))))
 
 (defmethod line ((cr cairo-renderer) x1 y1 x2 y2)
-  (update-stroke-color cr)
-  (c2:move-to x1 y1 (cr-context cr))
-  (c2:line-to x2 y2 (cr-context cr))
-  (c2:stroke (cr-context cr)))
+  (when (stroke-enabled cr)
+    (push-state cr)
+    (update-stroke-color cr)
+    (c2:move-to x1 y1 (cr-context cr))
+    (c2:line-to x2 y2 (cr-context cr))
+    (c2:stroke (cr-context cr))
+    (pop-state cr)))
 
 (defmethod rect ((cr cairo-renderer) x y width height)
+  (push-state cr)
   (c2:rectangle x y width height (cr-context cr))
+  (do-fill-stroke cr)
+  (pop-state cr))
+
+(defmethod ellipse ((cr cairo-renderer) x y width height)
+  (when (> 0 width)
+    (push-state cr)
+    (push-matrix cr)
+    (translate cr x y)
+    (scale cr 1 (/ height width))
+    (translate cr (- x) (- y))
+    (translate cr (* width 0.5) (* height 0.5))
+    (c2:arc x y width 0 (* pi 2) (cr-context cr))
+    (pop-matrix cr)
+    (do-fill-stroke cr)))
+
+(defmethod ellipse-c ((cr cairo-renderer) x y width height)
+  (when (> 0 width)
+    (push-state cr)
+    (push-matrix cr)
+    (translate cr x y)
+    (scale cr 1 (/ height width))
+    (translate cr (- x) (- y))
+    (c2:arc x y width 0 (* pi 2) (cr-context cr))
+    (pop-matrix cr)
+    (do-fill-stroke cr)
+    (pop-state cr)))
+
+
+;;; Paths
+
+(defmethod begin-path ((cr cairo-renderer))
+  (c2:new-path (cr-context cr)))
+
+(defmethod close-path ((cr cairo-renderer))
+  (c2:close-path (cr-context cr)))
+
+(defmethod draw-path ((cr cairo-renderer))
   (do-fill-stroke cr))
 
+(defmethod move-to ((cr cairo-renderer) x y)
+  (c2:move-to x y (cr-context cr)))
 
-;; Matrix operations
+(defmethod line-to ((cr cairo-renderer) x y)
+  (c2:line-to x y (cr-context cr)))
+
+(defmethod curve-to ((cr cairo-renderer) x1 y1 x2 y2 x3 y3)
+  (c2:curve-to x1 y1 x2 y2 x3 y3 (cr-context cr)))
+
+(defmethod arc ((cr cairo-renderer) x y radius angle1 angle2)
+  (c2:arc x y radius angle1 angle2 (cr-context cr)))
+
+
+
+
+;;; Matrix operations
 
 (defmethod push-matrix ((cr cairo-renderer))
   (vector-push-extend (c2:get-trans-matrix (cr-context cr)) (cr-matrix-stack cr)))
--- src/render/renderer.lisp
+++ src/render/renderer.lisp
@@ -10,23 +10,82 @@
    (stroke-color :accessor stroke-color
                  :initform (graphite.color:rgb 0 0 0))))
 
+;;; Renderer attributes
+
+(defgeneric set-line-cap (renderer cap-type)
+  (:documentation "Sets the rendering style for line endings.
+Possible options are:
+- `:butt' - start/stop the line exactly at the end point
+- `:square' - use a square line ending, with the center of the square being the start/end point
+- `:round' - use a round line ending, with the center of the circle being the start/end point"))
+
+(defgeneric set-line-join (renderer join-type)
+  (:documentation "Sets the line join style.
+Possible options are:
+- `:miter' - use a sharp corner
+- `:round' - use a round join, with the center of the circle at the joint point
+- `:bevel' - use a cut-off join, where the line is cut off at half the line width from the join point"))
+
 (defgeneric set-blend-mode (renderer mode)
-  (:documentation "Sets the current blend mode"))
+  (:documentation "Sets the current blend mode.
+Possible options are:
+- `:no-blend' - overrite the target with the source color
+- `:blend' - traditional alpha blending
+- `:add' - additive blending: src + dest'"))
 
 (defgeneric size (renderer)
   (:documentation "Returns the size of the canvas"))
 
+(defgeneric push-state (renderer)
+  (:documentation "Makes a copy of the current renderer state and saves it to an internal stack"))
+
+(defgeneric pop-state (renderer)
+  (:documentation "Restores the last saved state from the internal stack"))
+
+
+;;; 2D Primitives
+
 (defgeneric point (renderer x y)
-  (:documentation "Draw a point at the specified coordinates"))
+  (:documentation "Draw a point at X,Y"))
 
 (defgeneric line (renderer x1 y1 x2 y2)
-  (:documentation "Draw a line between the specified coordinates"))
+  (:documentation "Draw a line between X1,Y1 and X2,Y2"))
 
 (defgeneric rect (renderer x y width height)
-  (:documentation "Draw a rectangle at the specified coordinates"))
+  (:documentation "Draw a rectangle at X,Y with the given WIDTH and HEIGHT"))
 
 
-;; Matrix operations
+(defgeneric ellipse (renderer x y width height)
+  (:documentation "Draws an ellipse with the top left at X,Y with the given WIDTH and HEIGHT"))
+
+(defgeneric ellipse-c (renderer x y width height)
+  (:documentation "Draws an ellipse centered at X,Y with the given WIDTH and HEIGHT"))
+
+;;; Paths
+
+(defgeneric begin-path (renderer)
+  (:documentation "Clears the current path and starts a new one"))
+
+(defgeneric close-path (renderer)
+  (:documentation "Adds a line segment to the path from the current point to the first point in the path"))
+
+(defgeneric draw-path (renderer)
+  (:documentation "Draws the current path to the canvas using the current stroke and fill settings"))
+
+(defgeneric move-to (renderer x y)
+  (:documentation "Begin a new sub-path"))
+
+(defgeneric line-to (renderer x y)
+  (:documentation "Adds a line to the current path from the current point to X,Y"))
+
+(defgeneric curve-to (renderer x1 y1 x2 y2 x3 y3)
+  (:documentation "Adds a Bezier spline from the current point to position X3,Y3, using X1,Y1 and X2,Y2 as control points"))
+
+(defgeneric arc (renderer x y radius angle1 angle2)
+  (:documentation "Adds an arc of the given RADIUS to the current path, centered at X,Y. Begins at ANGLE1 and increases clockwise to ANGLE2"))
+
+
+;;; Matrix operations
 
 (defgeneric translate (renderer tx ty)
   (:documentation "Translates the origin by TX and TY"))
@@ -44,7 +103,7 @@
   (:documentation "Restores the transformation matrix from the stack"))
 
 
-;; Vector operations
+;;; Vector operations
 
 (defun point-v (renderer p)
   "Draw a point at the coordinates specified by the vector P"
@@ -56,3 +115,31 @@
   (with-aref (x1 y1) p1
     (with-aref (x2 y2) p2
       (line renderer x1 y1 x2 y2))))
+
+(defun rect-v (renderer origin size)
+  "Draw a rectangle with the top left corner at the vector ORIGIN with SIZE"
+  (rect renderer (v:x origin) (v:y origin) (v:x size) (v:y size)))
+
+(defun ellipse-v (renderer origin size)
+  "Draw an ellipse with the top left corner at the vector ORIGIN with SIZE"
+  (ellipse renderer (v:x origin) (v:y origin) (v:x size) (v:y size)))
+
+(defun ellipse-c-v (renderer origin size)
+  "Draw an ellipse with the center at the vector ORIGIN with SIZE"
+  (ellipse-c renderer (v:x origin) (v:y origin) (v:x size) (v:y size)))
+
+(defun move-to-v (renderer p)
+  "Begin a new sub-path at the vector P"
+  (move-to renderer (v:x p) (v:y p)))
+
+(defun line-to-v (renderer p)
+  "Adds a line to the current path from the current point to the vector P"
+  (line-to renderer (v:x p) (v:y p)))
+
+(defun curve-to-v (renderer c1 c2 p)
+  "Adds a Bezier spline from the current point to vector P, using vectors C1 and C2 as control points"
+  (curve-to renderer (v:x c1) (v:y c1) (v:x c2) (v:y c2) (v:x p) (v:y p)))
+
+(defun arc-v (renderer p radius angle1 angle2)
+  "Adds an arc of the given RADIUS to the current path centered at the vector P. Begins at ANGLE1 and increases clockwise to ANGLE2"
+  (arc renderer (v:x p) (v:y p) radius angle1 angle2))
